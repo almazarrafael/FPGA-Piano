0630b560d894f8c3f15397e5186c277655bb0db9
Rafael Almazar <47069450+almazarrafael@users.noreply.github.com>
2022-11-27 16:00:33 -0800

    almazarrafael: added rest and extended note

--- software/ROM_gen.py
+++ software/ROM_gen.py
@@ -1,30 +1,49 @@
 # Music ROM generator
-# TODO: Add rest in between each note
-# TODO: Add "extend" (idk music terms) and use char '-'
 
-print("Type in C,D,E,F,G,A,B for notes.\nType in x for rest.")
-str = input("Notes:")
-str = str.ljust(64, 'x')
-print(str)
+prev = ""
 
-for i in str:
-  if i == " ":
-    continue
-  elif i == "X" or i == "x":
+def main ():
+  global prev, j
+  print("Type in C,D,E,F,G,A,B for notes.\nType in x for rest.\nType in - for extending the note.\n")
+  str = input("Notes:")
+  str = str.ljust(64, 'x')
+  print(str)
+
+  for i in str:
+    decoder(i, prev)
+    prev = i
+    if (i == "-"):
+      pass
+    else:
+      print("0000000")
+
+  
+def decoder(note, prev):
+  if (note == '-'):
+    decoder(prev, "")
+    return
+
+  # Python doesn't have a switch case. L.
+  if note == " ":
+    pass
+  elif note == "X" or note == "x":
     print("0000000")
-  elif i == "C" or i == "c":
+  elif note == "C" or note == "c":
     print("1000000")
-  elif i == "D" or i == "d":
+  elif note == "D" or note == "d":
     print("0100000")
-  elif i == "E" or i == "e":
+  elif note == "E" or note == "e":
     print("0010000")
-  elif i == "F" or i == "f":
+  elif note == "F" or note == "f":
     print("0001000")
-  elif i == "G" or i == "g":
+  elif note == "G" or note == "g":
     print("0000100")
-  elif i == "A" or i == "a":
+  elif note == "A" or note == "a":
     print("0000010")
-  elif i == "B" or i == "b":
+  elif note == "B" or note == "b":
     print("0000001")
   else:
     print("0000000")
+
+if __name__ == "__main__":
+  main()
